fa8377f0e79e1c4ea7d9c92526f5df90199b079d
penrose15 <seongju99@naver.com>
2022-12-29 15:26:32 +0900

    chore :add corsconfig origin

---
+++ test.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=SC2164
+cd server2
+
+echo ">> grant permission"
+chmod +x gradlew
+
+echo ">> project clean Build Start"
+./gradlew clean build
+
+echo ">> pwd"
+pwd
+
+echo ">> go jar"
+cd ./build/libs
+
+JAR_NAME=$(ls|grep '0.0.1' | tail -n 1)
+echo "jar name : $JAR_NAME"
