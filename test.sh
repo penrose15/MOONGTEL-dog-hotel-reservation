4a971a0fda16d89a8d8ae4654396e7aa95d52410
penrose15 <seongju99@naver.com>
2022-12-29 16:46:31 +0900

    fix : test.sh 수정

--- test.sh
+++ test.sh
@@ -1,16 +1,29 @@
-# shellcheck disable=SC2164
-cd server2
+#!/bin/bash
 
-echo ">> grant permission"
-chmod +x gradlew
+BUILD_JAR=$(ls /home/ubuntu/jenkins/server2/build/libs/server-0.0.1-SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_JAR)
 
-echo ">> project clean Build Start"
-./gradlew clean build
+echo "> 현재 시간: $(date)" >> /home/ubuntu/jenkins/deploy.log
 
-echo ">> pwd"
-pwd
+echo "> build filename: $JAR_NAME" >> /home/ubuntu/jenkins/deploy.log
 
-cd /home/ubuntu
+echo "> build 파일 복사" >> /home/ubuntu/jenkins/deploy.log
+DEPLOY_PATH=/home/ubuntu/jenkins/
+cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">> go jar"
-cp build/libs/*.jar jenkins/
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/jenkins/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/jenkins/deploy.log
+else
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/jenkins/deploy.log
+  sudo kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/jenkins/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/jenkins/deploy.log 2>/home/ubuntu/jenkins/deploy_err.log &
